34f82683bf3688ebfa12ea9c9c7a12a936b30c62
tuteng <eguangning@gmail.com>
2019-03-06 20:24:12 +0800

    Support cors Mock login

--- front-end/config/dev.env.js
+++ front-end/config/dev.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API: '"https://easy-mock.com/mock/5c013a644ed9b43d7590d5a5/example"'
+  // BASE_API: '"//127.0.0.1:8080/admin/v2"'
 }
