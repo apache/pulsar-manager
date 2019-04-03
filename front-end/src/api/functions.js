981eb5abd18c759508225769d851d3fd5426420e
tuteng <eguangning@gmail.com>
2019-04-03 18:11:57 +0800

    Move BASE_URL_V2 to BASE_URL_V3

--- front-end/src/api/functions.js
+++ front-end/src/api/functions.js
@@ -1,24 +1,24 @@
 import request from '@/utils/request'
 
-const BASE_URL_V2 = '/admin/v3'
+const BASE_URL_V3 = '/admin/v3'
 
 export function fetchFunctions(tenant, namespace) {
   return request({
-    url: BASE_URL_V2 + `/functions/${tenant}/${namespace}`,
+    url: BASE_URL_V3 + `/functions/${tenant}/${namespace}`,
     method: 'get'
   })
 }
 
 export function fetchFunctionStats(tenant, namespace, functionName) {
   return request({
-    url: BASE_URL_V2 + `/functions/${tenant}/${namespace}/${functionName}/stats`,
+    url: BASE_URL_V3 + `/functions/${tenant}/${namespace}/${functionName}/stats`,
     method: 'get'
   })
 }
 
 export function fetchFunctionStatus(tenant, namespace, functionName) {
   return request({
-    url: BASE_URL_V2 + `/functions/${tenant}/${namespace}/${functionName}/status`,
+    url: BASE_URL_V3 + `/functions/${tenant}/${namespace}/${functionName}/status`,
     method: 'get'
   })
 }
@@ -27,7 +27,7 @@ export function createFunction(tenant, namespace, functionName, data) {
   return request({
     headers: { 'Content-Type': 'multipart/form-data' },
     // headers: { 'Content-Type': 'application/json' },
-    url: BASE_URL_V2 + `/functions/${tenant}/${namespace}/${functionName}`,
+    url: BASE_URL_V3 + `/functions/${tenant}/${namespace}/${functionName}`,
     method: 'post',
     data
   })
@@ -36,7 +36,7 @@ export function createFunction(tenant, namespace, functionName, data) {
 export function updateFunction(tenant, namespace, functionName, data) {
   return request({
     headers: { 'Content-Type': 'application/json' },
-    url: BASE_URL_V2 + `/functions/${tenant}/${namespace}/${functionName}`,
+    url: BASE_URL_V3 + `/functions/${tenant}/${namespace}/${functionName}`,
     method: 'put',
     data
   })
@@ -45,7 +45,7 @@ export function updateFunction(tenant, namespace, functionName, data) {
 export function deleteFunction(tenant, namespace, functionName) {
   return request({
     headers: { 'Content-Type': 'application/json' },
-    url: BASE_URL_V2 + `/functions/${tenant}/${namespace}/${functionName}`,
+    url: BASE_URL_V3 + `/functions/${tenant}/${namespace}/${functionName}`,
     method: 'delete'
   })
 }
@@ -53,7 +53,7 @@ export function deleteFunction(tenant, namespace, functionName) {
 export function restartFunctionInstance(tenant, namespace, functionName, instanceId) {
   return request({
     headers: { 'Content-Type': 'application/json' },
-    url: BASE_URL_V2 + `/functions/${tenant}/${namespace}/${functionName}/${instanceId}/restart`,
+    url: BASE_URL_V3 + `/functions/${tenant}/${namespace}/${functionName}/${instanceId}/restart`,
     method: 'post'
   })
 }
@@ -61,7 +61,7 @@ export function restartFunctionInstance(tenant, namespace, functionName, instanc
 export function startFunction(tenant, namespace, functionName) {
   return request({
     headers: { 'Content-Type': 'application/json' },
-    url: BASE_URL_V2 + `/functions/${tenant}/${namespace}/${functionName}/start`,
+    url: BASE_URL_V3 + `/functions/${tenant}/${namespace}/${functionName}/start`,
     method: 'post'
   })
 }
@@ -69,21 +69,21 @@ export function startFunction(tenant, namespace, functionName) {
 export function stopFunction(tenant, namespace, functionName) {
   return request({
     headers: { 'Content-Type': 'application/json' },
-    url: BASE_URL_V2 + `/functions/${tenant}/${namespace}/${functionName}/stop`,
+    url: BASE_URL_V3 + `/functions/${tenant}/${namespace}/${functionName}/stop`,
     method: 'post'
   })
 }
 
 export function fetchFunctionInstanceStats(tenant, namespace, functionName, instanceId) {
   return request({
-    url: BASE_URL_V2 + `/functions/${tenant}/${namespace}/${functionName}/${instanceId}/stats`,
+    url: BASE_URL_V3 + `/functions/${tenant}/${namespace}/${functionName}/${instanceId}/stats`,
     method: 'get'
   })
 }
 
 export function fetchFunctionInstanceStatus(tenant, namespace, functionName, instanceId) {
   return request({
-    url: BASE_URL_V2 + `/functions/${tenant}/${namespace}/${functionName}/${instanceId}/status`,
+    url: BASE_URL_V3 + `/functions/${tenant}/${namespace}/${functionName}/${instanceId}/status`,
     method: 'get'
   })
 }
@@ -91,7 +91,7 @@ export function fetchFunctionInstanceStatus(tenant, namespace, functionName, ins
 export function startFunctionInstance(tenant, namespace, functionName, instanceId) {
   return request({
     headers: { 'Content-Type': 'application/json' },
-    url: BASE_URL_V2 + `/functions/${tenant}/${namespace}/${functionName}/${instanceId}/start`,
+    url: BASE_URL_V3 + `/functions/${tenant}/${namespace}/${functionName}/${instanceId}/start`,
     method: 'post'
   })
 }
@@ -99,7 +99,7 @@ export function startFunctionInstance(tenant, namespace, functionName, instanceI
 export function stopFunctionInstance(tenant, namespace, functionName, instanceId) {
   return request({
     headers: { 'Content-Type': 'application/json' },
-    url: BASE_URL_V2 + `/functions/${tenant}/${namespace}/${functionName}/${instanceId}/stop`,
+    url: BASE_URL_V3 + `/functions/${tenant}/${namespace}/${functionName}/${instanceId}/stop`,
     method: 'post'
   })
 }
@@ -107,7 +107,7 @@ export function stopFunctionInstance(tenant, namespace, functionName, instanceId
 export function triggerFunction(tenant, namespace, functionName, data) {
   return request({
     headers: { 'Content-Type': 'multipart/form-data' },
-    url: BASE_URL_V2 + `/functions/${tenant}/${namespace}/${functionName}/trigger`,
+    url: BASE_URL_V3 + `/functions/${tenant}/${namespace}/${functionName}/trigger`,
     method: 'post',
     data
   })
