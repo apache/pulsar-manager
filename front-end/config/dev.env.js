c367dc1633374b963e6ad505da1c31100fc46612
tuteng <eguangning@gmail.com>
2019-04-09 17:05:07 +0800

    Move port to 9090 from 9000 fro prometheus

--- front-end/config/dev.env.js
+++ front-end/config/dev.env.js
@@ -3,5 +3,5 @@ module.exports = {
   ENV_CONFIG: '"dev"',
   // BASE_API: '"//127.0.0.1:443"'
   GRAFANA_ADDRESS: '"http://localhost:3000"',
-  PROMETHEUS_ADDRESS: '"http://localhost:9000"'
+  PROMETHEUS_ADDRESS: '"http://localhost:9090"'
 }
