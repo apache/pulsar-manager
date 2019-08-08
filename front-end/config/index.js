5d7f4e0adb5eb34bfd0b239611bc603c82053215
tuteng <eguangning@gmail.com>
2019-08-08 11:27:47 +0800

    Support set parameters use environment

--- front-end/config/index.js
+++ front-end/config/index.js
@@ -35,7 +35,7 @@ module.exports = {
 
     // can be overwritten by process.env.HOST
     // if you want dev by ip, please set host: '0.0.0.0'
-    host: 'localhost',
+    host: '0.0.0.0',
     port: 9527, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
