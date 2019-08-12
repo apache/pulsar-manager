699f66650339f42b1a12f85ca75d65e0a9eb2b23
Sijie Guo <sijie@apache.org>
2019-08-12 10:15:31 +0800

    Rename package `com.manager.pulsar` to `io.streamnative.pulsar.manager`

---
+++ src/main/java/io/streamnative/pulsar/manager/controller/LoginController.java
@@ -0,0 +1,93 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.streamnative.pulsar.manager.controller;
+
+import com.google.common.collect.Maps;
+import io.streamnative.pulsar.manager.service.JwtService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+/**
+ * User login and logout rest api.
+ */
+@RequestMapping(value = "/pulsar-manager")
+@Api(description = "Support user login and logout.")
+@Validated
+@RestController
+public class LoginController {
+
+    @Value("${pulsar-manager.account}")
+    private String account;
+
+    @Value("${pulsar-manager.password}")
+    private String password;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @ApiOperation(value = "Login pulsar manager")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "ok"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
+    @RequestMapping(value = "/login", method =  RequestMethod.POST)
+    public ResponseEntity<Map<String, Object>> login(
+            @RequestBody Map<String, String> body) {
+        String userAccount = body.get("username");
+        String userPassword = body.get("password");
+        Map<String, Object> result = Maps.newHashMap();
+        if (userAccount != account && userPassword != password) {
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            String token = jwtService.toToken(account + "-" + password);
+            result.put("login", "success");
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("token", token);
+            jwtService.setToken(request.getSession().getId(), token);
+            return new ResponseEntity(result, headers, HttpStatus.OK);
+        }
+        result.put("login", "error");
+        return ResponseEntity.ok(result);
+    }
+
+    @ApiOperation(value = "Logout pulsar manager")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "ok"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
+    @RequestMapping(value = "/logout", method =  RequestMethod.POST)
+    public ResponseEntity<Map<String, Object>> logout() {
+        Map<String, Object> result = Maps.newHashMap();
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        result.put("logout", "success");
+        jwtService.removeToken(request.getSession().getId());
+        return ResponseEntity.ok(result);
+    }
+}
---
+++ src/main/java/io/streamnative/pulsar/manager/mapper/TopicsStatsMapper.java
@@ -0,0 +1,83 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.streamnative.pulsar.manager.mapper;
+
+import com.github.pagehelper.Page;
+import io.streamnative.pulsar.manager.entity.TopicStatsEntity;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface TopicsStatsMapper {
+
+    @Insert("INSERT INTO topicsStats(environment, cluster,broker,tenant,namespace,bundle,persistent,topic," +
+            "producerCount,subscriptionCount,msgRateIn,msgThroughputIn,msgRateOut,msgThroughputOut," +
+            "averageMsgSize,storageSize,timestamp) " +
+            "VALUES(#{environment},#{cluster},#{broker},#{tenant},#{namespace},#{bundle},#{persistent},#{topic}," +
+            "#{producerCount},#{subscriptionCount},#{msgRateIn},#{msgThroughputIn},#{msgRateOut},#{msgThroughputOut}," +
+            "#{averageMsgSize},#{storageSize},#{timestamp})")
+    @Options(useGeneratedKeys=true, keyProperty="topicStatsId", keyColumn="topicStatsId")
+    void insert(TopicStatsEntity topicStatsEntity);
+
+    @Select("SELECT topicStatsId,environment,cluster,broker,tenant,namespace,bundle,persistent,topic,producerCount,subscriptionCount," +
+            "msgRateIn,msgThroughputIn,msgRateOut,msgThroughputOut,averageMsgSize,storageSize,timestamp FROM topicsStats " +
+            "ORDER BY timestamp DESC limit 1 ")
+    TopicStatsEntity findMaxTime();
+
+    @Select("SELECT topicStatsId,environment,cluster,broker,tenant,namespace,bundle,persistent,topic,producerCount,subscriptionCount," +
+            "msgRateIn,msgThroughputIn,msgRateOut,msgThroughputOut,averageMsgSize,storageSize,timestamp FROM topicsStats " +
+            "WHERE environment=#{environment} and cluster=#{cluster} and broker=#{broker} and timestamp=#{timestamp}")
+    Page<TopicStatsEntity> findByClusterBroker(
+            @Param("environment") String environment,
+            @Param("cluster") String cluster,
+            @Param("broker") String broker,
+            @Param("timestamp") long timestamp);
+
+    @Select("SELECT topicStatsId,environment,cluster,tenant,namespace,bundle,persistent,topic,producerCount,subscriptionCount," +
+            "msgRateIn,msgThroughputIn,msgRateOut,msgThroughputOut,averageMsgSize,storageSize,timestamp FROM topicsStats " +
+            "WHERE cluster=#{cluster} and environment=#{environment} and tenant=#{tenant} and namespace=#{namespace} " +
+            "and timestamp=#{timestamp}")
+    Page<TopicStatsEntity> findByNamespace(
+            @Param("environment") String environment,
+            @Param("cluster") String cluster,
+            @Param("tenant") String tenant,
+            @Param("namespace") String namespace,
+            @Param("timestamp") long timestamp);
+
+    @Select({"<script>",
+            "SELECT environment, cluster, tenant, namespace, persistent, topic,"
+                + "sum(producerCount) as producerCount,"
+                + "sum(subscriptionCount) as subscriptionCount,"
+                + "sum(msgRateIn) as msgRateIn,"
+                + "sum(msgThroughputIn) as msgThroughputIn,"
+                + "sum(msgRateOut) as msgRateOut,"
+                + "sum(msgThroughputOut) as msgThroughputOut,"
+                + "avg(averageMsgSize) as averageMsgSize,"
+                + "sum(storageSize) as storageSize, timestamp FROM topicsStats",
+            "WHERE environment=#{environment} and tenant=#{tenant} and namespace=#{namespace} and timestamp=#{timestamp} and " +
+                    "topic IN <foreach collection='topicList' item='topic' open='(' separator=',' close=')'> #{topic} </foreach>" +
+            "GROUP BY cluster, persistent, topic" +
+            "</script>"})
+    Page<TopicStatsEntity> findByMultiTopic(
+            @Param("environment") String environment,
+            @Param("tenant") String tenant,
+            @Param("namespace") String namespace,
+            @Param("persistent") String persistent,
+            @Param("topicList") List<String> topicList,
+            @Param("timestamp") long timestamp);
+
+    @Delete("DELETE FROM topicsStats WHERE #{nowTime} - #{timeInterval} >= timestamp")
+    void delete(@Param("nowTime") long nowTime, @Param("timeInterval") long timeInterval);
+}
